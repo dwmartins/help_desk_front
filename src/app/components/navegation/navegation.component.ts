a87f363b97980da3d66d3f7d79ccd6a0eba232c4
Douglas Martins <douglas5422@outlook.com>
2023-08-19 22:43:09 -0300

    feat: finalizando o componente de navegação

--- src/app/components/navegation/navegation.component.ts
+++ src/app/components/navegation/navegation.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-navegation',
   templateUrl: './navegation.component.html',
   styleUrls: ['./navegation.component.css']
 })
-export class NavegationComponent {
+export class NavegationComponent implements OnInit{
+  @ViewChild('nav', { static: true }) navElement!: ElementRef
+  @ViewChild('main', { static: true }) mainElement!: ElementRef
 
+  statusNav: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.getWindowSize();
+  }
+
+  closeNav() {
+    const nav = this.navElement.nativeElement;
+    const main = this.mainElement.nativeElement;
+
+    nav.classList.toggle('close');
+    main.classList.toggle('close_main');
+
+    this.statusNav = !this.statusNav;
+    console.log(this.statusNav);
+  }
+
+  getWindowSize() {
+    if(window.innerWidth <= 500) {
+      this.navElement.nativeElement.classList.add('close');
+      this.mainElement.nativeElement.classList.add('close_main');
+      console.log('fechado')
+    }
+  }
 }
