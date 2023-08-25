f979be79dd27c5ea94c203e81c992daccdc67752
Douglas Martins <douglas5422@outlook.com>
2023-08-25 20:36:54 -0300

    feat: Validando formulario

--- src/app/components/new-called/new-called.component.ts
+++ src/app/components/new-called/new-called.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-called',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-called.component.css']
 })
 export class NewCalledComponent {
+
+  formNewCalled: FormGroup;
+
   editorContent: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.formNewCalled = this.formBuilder.group({
+      user_id: ['1'],
+      titulo: ['', Validators.required],
+      descricao: ['', Validators.required],
+      prioridade: ['', Validators.required],
+      categoria: ['', Validators.required]
+    });
+  }
+
+  submitForm() {
+    if(this.formNewCalled.valid) {
+      console.log('valido');
+      console.log(this.formNewCalled.value);
+      this.formNewCalled.reset();
+    } else {
+      console.log('não validado');
+      this.formNewCalled.markAllAsTouched();
+    }
+  }
 }
