92bee4d242aa2c99f0f5a8e30abda042a65db62b
Douglas Martins <douglas5422@outlook.com>
2023-08-26 14:20:39 -0300

    fix: Corrigindo a interface de chamados

--- src/app/models/called/Called.ts
+++ src/app/models/called/Called.ts
@@ -1,9 +1,12 @@
 export interface Called {
+    chamado_id: number,
     user_id: number,
     titulo: string,
     descricao: string,
     prioridade: string,
-    categoria: string
+    categoria: string,
+    createdAt: Date,
+    updateAt: Date
 
     // res
     success: boolean,
